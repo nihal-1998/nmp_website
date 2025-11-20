e55470198e17f86cce07ebc31adb41fa8d97632e
Nihal_Patel_1998 <nihalhari41@gmail.com>
2025-11-20 10:48:29 +0600

    Update baseUrl.ts

--- src/app/utils/baseUrl.ts
+++ src/app/utils/baseUrl.ts
@@ -1,4 +1,4 @@
-export const BASE_URL="http://13.61.205.202:9090/api/v1"
+export const BASE_URL="http://13.61.89.160:9090/api/v1"
 // export const BASE_URL="https://backend.onlinecornerstore.com/api/v1"
 //export const BASE_URL="https://nmp-backend.vercel.app/api/v1"
 
